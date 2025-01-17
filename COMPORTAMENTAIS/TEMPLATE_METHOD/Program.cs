433d16e28670e12dbfb070aa1018ee8b04fd9371
Alexandre R. Gomes <alexandrerodgomes@hotmail.com>
2025-01-17 16:41:56 -0300

    feate: Criacao codigo Comum Template Method.

--- COMPORTAMENTAIS/TEMPLATE_METHOD/Program.cs
+++ COMPORTAMENTAIS/TEMPLATE_METHOD/Program.cs
@@ -4,3 +4,19 @@
     O Template Method permite que as subclasses redefinam certos passos de um algoritmo
     sem alterar a estrutura do mesmo.
 */
+using TEMPLATE_METHOD.MODO_COMUM;
+
+var valor = 1000;
+ Gateway gateway = new();
+
+ Console.WriteLine("Crédito");
+ var pagamentoCredito = new PagamentoCredito(valor, gateway);
+ pagamentoCredito.RealizarCobranca();
+
+Console.WriteLine("Dédito");
+var pagamentoDebito = new PagamentoDebito(valor, gateway);
+pagamentoDebito.RealizarCobranca();
+
+Console.WriteLine("Dinheiro");
+var pagamentoDinheiro = new PagamentoDinheiro(valor, gateway);
+pagamentoDinheiro.RealizarCobranca();
