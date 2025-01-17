d3d66001211574759eafec984011ecae7f02452a
Alexandre R. Gomes <alexandrerodgomes@hotmail.com>
2025-01-17 15:51:45 -0300

    feat: Conceito do padrao Template Method.

--- COMPORTAMENTAIS/TEMPLATE_METHOD/Program.cs
+++ COMPORTAMENTAIS/TEMPLATE_METHOD/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+    O padrão Template Method define o esqueleto de um algoritmo dentro de um método, 
+    transferindo alguns de seus passos para subclasses.
+    O Template Method permite que as subclasses redefinam certos passos de um algoritmo
+    sem alterar a estrutura do mesmo.
+*/
