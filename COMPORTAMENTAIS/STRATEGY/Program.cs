53f2376b2b7d6efded9cabc2cbe9a17192a01924
Alexandre R. Gomes <alexandrerodgomes@hotmail.com>
2025-01-17 15:51:20 -0300

    feat: Conceito do padrao Strategy.

--- COMPORTAMENTAIS/STRATEGY/Program.cs
+++ COMPORTAMENTAIS/STRATEGY/Program.cs
@@ -1,4 +1,8 @@
-﻿
+﻿/*
+    O padrão de projeto Strategy define uma família de algoritmos, 
+    encapsula cada um deles e os torna intercambiáveis. 
+    O Strategy permite que o algoritmo varie independentemente dos clientes que o utilizam.
+*/
 
 using STRATEGY.MODO_COMUM;
 using STRATEGY.MODO_PADRAO;
