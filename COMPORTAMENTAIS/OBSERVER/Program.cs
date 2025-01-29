fca3c2c50a7082f0bc4fda25967b697322266c2f
Alexandre R. Gomes <alexandrerodgomes@hotmail.com>
2025-01-29 08:50:02 -0300

    feat: Criacao projeto observer.

---
+++ COMPORTAMENTAIS/OBSERVER/Program.cs
@@ -0,0 +1,6 @@
+﻿/*
+    O Observer é um padrão de projeto de software que define 
+    uma dependência um-para-muitos entre objetos, 
+    de modo que quando um objeto muda seu estado, 
+    todos seus dependentes são notificados e atualizados automaticamente.
+*/
