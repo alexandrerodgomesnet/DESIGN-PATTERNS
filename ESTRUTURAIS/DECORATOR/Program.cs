e6f513b08e806e49c0e16488f0059db5442e6b76
Alexandre R. Gomes <alexandrerodgomes@hotmail.com>
2025-02-08 09:21:47 -0300

    feat: Criacao do projeto decorator.

---
+++ ESTRUTURAIS/DECORATOR/Program.cs
@@ -0,0 +1,5 @@
+﻿/*
+    O Padrão Decorator anexa responsabilidades adicionais a um objeto dinamicamente. 
+    Os Decorators fornecem uma alternativa flexível ao uso de 
+    subclasses para extensão de funcionalidades.
+*/
