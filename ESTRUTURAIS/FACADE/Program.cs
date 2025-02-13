195d8f86b157a50862a9a4259e8d2a87712b016e
Alexandre R. Gomes <alexandrerodgomes@hotmail.com>
2025-02-13 08:58:25 -0300

    feat: Criacao projeto facade.

---
+++ ESTRUTURAIS/FACADE/Program.cs
@@ -0,0 +1,4 @@
+﻿/*
+    O padrão Facade fornece uma interface unificada para um conjunto de interfaces em um subsistema. 
+    O Facade define uma interface de nível mais alto que facilita a utilização do subsistema.
+*/
