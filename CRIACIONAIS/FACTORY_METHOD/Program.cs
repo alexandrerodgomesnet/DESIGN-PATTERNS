31783bf94acdb4212ac45afbe247f244d1f097dc
Alexandre R. Gomes <alexandrerodgomes@hotmail.com>
2025-02-13 13:14:37 -0300

    feat: Criando projeto Factory Method.

---
+++ CRIACIONAIS/FACTORY_METHOD/Program.cs
@@ -0,0 +1,5 @@
+﻿/*
+    O Padrão Factory Method define uma interface para criar um objeto, 
+    mas permite que a subclasses possam decidir qual classe instanciar, 
+    possibilitando que uma classe seja capaz de prorrogar a instanciação de uma classe para subclasses.
+*/
