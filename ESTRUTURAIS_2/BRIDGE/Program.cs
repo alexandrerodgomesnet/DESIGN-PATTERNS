d7c4e90c6c6ea0a22753d05bbcae61e0c21b6fb9
Alexandre R. Gomes <alexandrerodgomes@hotmail.com>
2025-03-07 16:17:37 -0300

    feat: Criacao projeto padrao Bridge.

---
+++ ESTRUTURAIS_2/BRIDGE/Program.cs
@@ -0,0 +1,6 @@
+﻿/*
+    ​O Padrão Bridge desacopla uma abstração de sua implementação, ou seja, 
+    permite dividir uma classe grande ou um conjunto de classes diretamente 
+    ligadas em duas hierarquias separadas, deste modo as duas hierarquias 
+    podem variar de forma independente.
+*/
