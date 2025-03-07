e86a3defc0e5c8d6ee34e8a2e904aaf63c5e058c
Alexandre R. Gomes <alexandrerodgomes@hotmail.com>
2025-03-06 23:39:56 -0300

    feat: Criacao padrao Command.

---
+++ COMPORTAMENTAIS_2/COMMAND/Program.cs
@@ -0,0 +1,5 @@
+﻿/*
+    O padrão de projeto Command encapsula uma solicitação como um objeto, 
+    isso lhe permite parametrizar clientes com diferentes solicitações, 
+    enfileirar ou registrar (log) solicitações e suportar solicitações que podem ser desfeitas.
+*/
