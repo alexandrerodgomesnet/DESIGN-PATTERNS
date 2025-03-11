80bd9eca4a2f9e8722f32e61cae1b0ddbf9afa58
Alexandre R. Gomes <alexandrerodgomes@hotmail.com>
2025-03-11 15:20:31 -0300

    feat: Criacao projeto Visitor.

---
+++ COMPORTAMENTAIS_3/VISITOR/Program.cs
@@ -0,0 +1,6 @@
+﻿/*
+    O padrão de projeto Visitor representa uma operação a ser executada 
+    nos membros de uma estrutura de objetos. 
+    Ele permite definir uma nova operação sem mudar as classes 
+    dos membros sobre quais opera.
+*/
